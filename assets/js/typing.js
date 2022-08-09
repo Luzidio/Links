0525414597d88053e61df84335d7b4a411f672d4
Luzídio Rafael <48757536+Luzidio@users.noreply.github.com>
2022-08-09 08:43:00 +0200

    Add files via upload

---
+++ assets/js/typing.js
@@ -0,0 +1,7 @@
+/*========typing animation============================= */
+var typed = new Typed('.typing', {
+  strings: ['', 'Programação WEB', 'Javascript', 'React Js'],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
